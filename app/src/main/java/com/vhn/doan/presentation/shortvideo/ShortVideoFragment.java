d5b88d0de9a02dd9d3ba2670d38236d6ac3e2007
Vũ Hoài Nam <20212501@eaut.edu.vn>
2025-08-09 17:01:17 +0700

    Revert "Improve short video interactions"

--- app/src/main/java/com/vhn/doan/presentation/shortvideo/ShortVideoFragment.java
+++ app/src/main/java/com/vhn/doan/presentation/shortvideo/ShortVideoFragment.java
@@ -275,8 +275,7 @@ public class ShortVideoFragment extends Fragment implements ShortVideoContract.V
                         // Play video mới (chỉ khi fragment visible)
                         currentPosition = newPosition;
                         if (isFragmentVisible && isVideoPlayerReady) {
-                            // Delay playback until RecyclerView has completed layout for the new item
-                            recyclerView.post(() -> adapter.playVideoAt(currentPosition));
+                            adapter.playVideoAt(currentPosition);
                         }
 
                         // Thông báo presenter về việc xem video
